d5ce65877c45a78b101c7448efb9d2fc7b87c2e4
Mark Franco <marfra@microsoft.com>
2024-02-10 12:37:44 -0500

    changed some styling

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export const App = () => {
       headers: {},
       additionalBodyProps: {}
     }}
-    style={{ borderRadius: 8, width: '100%' }}
+    style={{ borderRadius: 8, width: '100%', height: '100%' }}
   ></DeepChat> 
       
    </div>
