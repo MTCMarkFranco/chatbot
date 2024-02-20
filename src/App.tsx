d36650a3bbee48bbd3a65f3c4ee07b3d8a83f786
Mark Franco <marfra@microsoft.com>
2024-02-19 20:24:40 -0500

    working code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,60 @@
 import { DeepChat } from 'deep-chat-react';
 import './App.css';
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import RecordTable from './RecordTable.tsx';
 import SynthesisTable from './SynthesisTable.tsx';
+import React, { useRef } from 'react';
 
 export const App = () => {
+  
+  const apiUrl = "http://192.168.2.159:5000/chat";
+  const lastQueryResults = useRef(null);
   const initialMessages = [
     { role: 'user', text: 'Hi, what would you like to search on?' },
   ];
+  lastQueryResults.current = null;
+
+  const requestInterceptor = (request: any) => {
+        
+    if(lastQueryResults.current != null)    
+      request.body.records = lastQueryResults.current;
+    return request;
+  }
   
   const responseInterceptor = (response: any) => {
   
     let chatResponse = '';
-    
-    if (response.records) {
-      chatResponse = ReactDOMServer.renderToString(<RecordTable records={response.records} />);
+    let synthesisButton = '';
 
-    } else if (response.synthesis) {
-      chatResponse = ReactDOMServer.renderToString(<SynthesisTable synthesis={response.synthesis} />);
-    } else if (response.None) {
-      chatResponse = "<p>Ok, Let's start a new search. What would you like to search on?</p>";
-    }
+    if (response.records) {
+      lastQueryResults.current = response.records;
+      chatResponse = ReactDOMServer.renderToString(<RecordTable records={response.records} />);
+      synthesisButton = "<div class='deep-chat-temporary-message'><button class='deep-chat-button deep-chat-suggestion-button' style='margin-top: 5px'>Generate Synthesis?</button></div>";
+    } 
     
-    return {html: chatResponse};
+    if (response.synthesis) {
+      chatResponse = ReactDOMServer.renderToString(<SynthesisTable synthesis={response.synthesis} />);
+      lastQueryResults.current = null;
+    }  
+
+    return {html: chatResponse + synthesisButton};
   }
 
   return (
     
     <div className="App">
-    <DeepChat
-    initialMessages={initialMessages}
-    request={{
-      url: "http://localhost:5000/query",
-      method: "POST",
-      headers: {},
-      additionalBodyProps: {}
-    }}
-    style={{ borderRadius: 8, width: '100%', height: '100%' }}
-    responseInterceptor={responseInterceptor}
-  ></DeepChat> 
+      <DeepChat 
+      initialMessages={initialMessages}
+      request={{
+        url: apiUrl,
+        method: "POST",
+        headers: {},
+        additionalBodyProps: {records: lastQueryResults.current}
+      }}
+      style={{ borderRadius: 8, width: '100%', height: '100%' }}
+      responseInterceptor={responseInterceptor}
+      requestInterceptor={requestInterceptor}
+      ></DeepChat> 
       
    </div>
 
