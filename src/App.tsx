09a002b2d93f12016e39c099f03683f3af2dddfa
Mark Franco <marfra@microsoft.com>
2024-03-05 22:12:45 -0500

    Line 59 still doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { DeepChat } from 'deep-chat-react';
+import { DeepChat } from 'deep-chat-react-dev';
 import './App.css';
 import ReactDOMServer from 'react-dom/server';
 import RecordTable from './RecordTable.tsx';
@@ -12,15 +12,14 @@ export const App = () => {
   const lastQueryResults = useRef(null);
   const synthesisData = useRef('');
   const deepchatref = useRef(null);
-  const initialMessages = [
-    { role: 'user', text: 'Hi, what would you like to search on?' },
-  ];
+  const initialMessages = { text: 'Hi, what would you like to search on?' }
   lastQueryResults.current = null;
     
-  const request = {
+  const connect = {
     url: apiUrl,
     websocket: true,
     additionalBodyProps: {records: lastQueryResults.current},
+    stream: {simulation: 'stop-key'},
     handler: async (body, signals) => {
     try {
         const websocket = new WebSocket(apiUrl);
@@ -33,32 +32,46 @@ export const App = () => {
           let chatResponse = '';
           let synthesisButton = '';
           
-          if (response.records && response.records.length > 0) {
-            lastQueryResults.current = response.records;
-            chatResponse = ReactDOMServer.renderToString(<RecordTable records={response.records} />);
-            synthesisData.current = '';
-            synthesisButton = "<div class='deep-chat-temporary-message'><button class='deep-chat-button deep-chat-suggestion-button' style='margin-top: 5px'>Generate Synthesis</button></div>";
-            if(chatResponse === '') {
-              chatResponse = 'No records found... Please try again.';
-            }
-  
-          const remarkable = new Remarkable('full', {
-            html: true,
-            typographer: true,
-            });
-
-          const htmlResponse = remarkable.render(chatResponse + synthesisButton,{});
-          const highlightedResponse = htmlResponse.replace(/<em>/g, '<mark style="background-color: yellow;">').replace(/<\/em>/g, '</mark>');
-          signals.onResponse({html: highlightedResponse, overwrite: false});
-      
-          } else if (response) {
-            synthesisData.current += response.text;
-            chatResponse = ReactDOMServer.renderToString(<SynthesisTable synthesis={ synthesisData.current} />);
-            lastQueryResults.current = null;
-
-            signals.onResponse({html: chatResponse, overwrite: true});
+          // If Stop Key is received, stop the stream
+          if (response.text === 'stop-key')
+          {
+            signals.onResponse(response);
           }
+          else
+          {
+          
+            if (response.records && response.records.length > 0) {
+              lastQueryResults.current = response.records;
+              chatResponse = ReactDOMServer.renderToString(<RecordTable records={response.records} />);
+              synthesisData.current = '';
+              synthesisButton = "<div class='deep-chat-temporary-message'><button class='deep-chat-button deep-chat-suggestion-button' style='margin-top: 5px'>Generate Synthesis</button></div>";
+              if(chatResponse === '') {
+                chatResponse = 'No records found... Please try again.';
+              }
+    
+              const remarkable = new Remarkable('full', {
+                html: true,
+                typographer: true,
+                });
+
+              const htmlResponse = remarkable.render(chatResponse + synthesisButton,{});
+              const highlightedResponse = htmlResponse.replace(/<em>/g, '<mark style="background-color: yellow;">').replace(/<\/em>/g, '</mark>');
+              signals.onResponse({html: highlightedResponse, overwrite: false});
+        
+            } 
+            else {
+              
+              if (response) {
+              synthesisData.current += response.text;
+              chatResponse = ReactDOMServer.renderToString(<SynthesisTable synthesis={ synthesisData.current} />);
+              lastQueryResults.current = null;
+              signals.onResponse({html: chatResponse, overwrite: false});
+
+              }
+            }
+        }
       }
+    
       websocket.onclose = () => {
         signals.onClose(); // stops the user from sending messages
       };
@@ -88,24 +101,20 @@ export const App = () => {
 
   return (
     
-    <div className="App">
-      <DeepChat ref={deepchatref} 
-      initialMessages={initialMessages}
-      request={request}
-      style={{
-        width: '100%',
-        height: '100%',
-        borderRadius: '8px',
-        borderColor: '#e4e4e4', 
-        // background: 'linear-gradient(90deg, rgb(239, 242, 247) 0%, 7.60286%, rgb(237, 240, 249) 15.2057%, 20.7513%, rgb(235, 239, 248) 26.297%, 27.6386%, rgb(235, 239, 248) 28.9803%, 38.2826%, rgb(231, 237, 249) 47.585%, 48.1216%, rgb(230, 236, 250) 48.6583%, 53.1306%, rgb(228, 236, 249) 57.6029%, 61.5385%, rgb(227, 234, 250) 65.4741%, 68.7835%, rgb(222, 234, 250) 72.093%, 75.7603%, rgb(219, 230, 248) 79.4275%, 82.8265%, rgb(216, 229, 248) 86.2254%, 87.8354%, rgb(213, 228, 249) 89.4454%, 91.8605%, rgb(210, 226, 249) 94.2755%, 95.4383%, rgb(209, 225, 248) 96.6011%, 98.3005%, rgb(208, 224, 247) 100%)'
-      }}
-      // responseInterceptor={responseInterceptor}
-      // requestInterceptor={requestInterceptor}
-      stream={true}
-      
-      ></DeepChat> 
-      
-   </div>
+      <div className="App">
+        <DeepChat ref={deepchatref} 
+        introMessage={initialMessages}
+        connect={connect}
+        style={{
+          width: '100%',
+          height: '100%',
+          borderRadius: '8px',
+          borderColor: '#e4e4e4', 
+          // background: 'linear-gradient(90deg, rgb(239, 242, 247) 0%, 7.60286%, rgb(237, 240, 249) 15.2057%, 20.7513%, rgb(235, 239, 248) 26.297%, 27.6386%, rgb(235, 239, 248) 28.9803%, 38.2826%, rgb(231, 237, 249) 47.585%, 48.1216%, rgb(230, 236, 250) 48.6583%, 53.1306%, rgb(228, 236, 249) 57.6029%, 61.5385%, rgb(227, 234, 250) 65.4741%, 68.7835%, rgb(222, 234, 250) 72.093%, 75.7603%, rgb(219, 230, 248) 79.4275%, 82.8265%, rgb(216, 229, 248) 86.2254%, 87.8354%, rgb(213, 228, 249) 89.4454%, 91.8605%, rgb(210, 226, 249) 94.2755%, 95.4383%, rgb(209, 225, 248) 96.6011%, 98.3005%, rgb(208, 224, 247) 100%)'
+        }}
+        ></DeepChat> 
+        
+     </div>
 
   );
 }
